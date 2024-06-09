809239b96e45e193a6bec2b7bd4e28ed26353bc2
Gabriel <gabrielgomesch@gmail.com>
2024-06-09 13:08:42 -0300

    clean unneeded laravel componnts

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "
+        Run this app on CLI with
+
+        <span style=\"
+            background-color: #444;
+            color: #eee;
+            padding: 4px;
+            font-weight: bold;
+            font-family: 'Courier New';
+        \">php artisan roach:run ReadOnePieceCom</span>
+        ";
 });
